41ac12890a4fc16f460d3cb95765b25f6a7d6eb3
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-04-13 11:16:25 -0400

    Fix is_multi_region_trail var

--- main.tf
+++ main.tf
@@ -161,7 +161,7 @@ resource "aws_cloudtrail" "ct" {
   enable_logging                = "${var.enable_logging}"
   enable_log_file_validation    = "${var.enable_log_file_validation}"
   include_global_service_events = "${var.include_global_service_events}"
-  is_multi_region_trail         = "${var.enable_log_file_validation}"
+  is_multi_region_trail         = "${var.is_multi_region_trail}"
   cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.ct.arn}"
   cloud_watch_logs_role_arn     = "${aws_iam_role.ct.arn}"
   depends_on                    = ["aws_s3_bucket_policy.bucket"]
